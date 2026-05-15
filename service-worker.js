c0722e66be45c944a3cd5dc6d4587df78335cc76
jhim24 <85859168+jhim24@users.noreply.github.com>
2026-05-15 18:22:18 +0300

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "papprito-hr-v1";
+
+const urlsToCache = [
+
+"/",
+"/index.html",
+"/employee.html",
+"/attendance.html",
+"/payroll.html",
+"/employeeportal.html",
+"/hrapproval.html",
+"/payslip.html"
+
+];
+
+self.addEventListener("install",event=>{
+
+event.waitUntil(
+
+caches.open(CACHE_NAME)
+.then(cache=>{
+
+return cache.addAll(urlsToCache);
+
+})
+
+);
+
+});
+
+self.addEventListener("fetch",event=>{
+
+event.respondWith(
+
+caches.match(event.request)
+.then(response=>{
+
+return response || fetch(event.request);
+
+})
+
+);
+
+});
